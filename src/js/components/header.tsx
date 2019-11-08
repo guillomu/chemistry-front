f787a8cb3e2f474cdca71f311b34ce410ae6fae9
GuiGui <groult_guigui@hotmail.com>
2019-11-09 00:36:42 +0100

    [Header] Adding Header component

---
+++ src/js/components/header.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="header">
+        <i className="logo"></i>
+        <h1>Header</h1>
+        <div className="profile">
+          <i></i>
+        </div>
+      </header>
+    );
+  }
+}
+export default Header;
